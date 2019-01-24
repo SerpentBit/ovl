58dc0d08edef8509649de0c7e4f8de0f822bfe14
Elysium 1937 <45563197+1937Elysium@users.noreply.github.com>
2019-01-25 01:56:41 +0200

    Updated code to the latest version

--- Code/Sorters.py
+++ Code/Sorters.py
@@ -1,13 +1,13 @@
 # Copyright 2018 Ori Ben-Moshe - All rights reserved.
 import cv2
-import Geometry
+from . import Geometry
 from sys import version_info
 
 if version_info[0] == 3:
     xrange = range
 
 
-def descending_area_sort(contour_list):
+def dec_area_sort(contour_list):
     """
     Action: sorts the list of contours from the largest to the smallest based on area of the contour
     :param contour_list: List of Contours to filter
@@ -20,7 +20,7 @@ def descending_area_sort(contour_list):
     return contour_list
 
 
-def ascending_area_sort(contour_list):
+def inc_area_sort(contour_list):
     """
     Action: sorts the list of contours from the smallest to the largest based on area of the contou
     :param contour_list: List of Contours to filter
@@ -30,7 +30,7 @@ def ascending_area_sort(contour_list):
     if type(contour_list) is not list:
         return [contour_list]
     contour_list = sorted(contour_list, key=lambda x: cv2.contourArea(x))
-    return contour_list[::-1]
+    return contour_list
 
 
 def circle_sort(contour_list, area_limit=0.9, radius_limit=0.8):
@@ -46,4 +46,30 @@ def circle_sort(contour_list, area_limit=0.9, radius_limit=0.8):
     if type(contour_list) is not list:
         return [contour_list]
     contour_list = sorted(contour_list, key=lambda x: Geometry.circle_rating(x, area_limit, radius_limit))
+    return contour_list
+
+
+def dec_length_sort(contour_list):
+    """
+    Action: sorts the list of contours from the largest to the smallest based on area of the contour
+    :param contour_list: List of Contours to filter
+    :type contour_list: List or one contour (ndarray)
+    :return: the contour list sorted.
+    """
+    if type(contour_list) is not list:
+        return [contour_list]
+    contour_list = sorted(contour_list, key=Geometry.open_arc_length)[::-1]
+    return contour_list
+
+
+def inc_length_sort(contour_list):
+    """
+    Action: sorts the list of contours from the smallest to the largest based on area of the contour
+    :param contour_list: List of Contours to filter
+    :type contour_list: List or one contour (ndarray)
+    :return: the contour list sorted.
+    """
+    if type(contour_list) is not list:
+        return [contour_list]
+    contour_list = sorted(contour_list, key=Geometry.open_arc_length)
     return contour_list
