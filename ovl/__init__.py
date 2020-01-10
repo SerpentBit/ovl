c21a33804aeb0924679588b3feb56b00f9b99f2b
1937Elysium <frcelysium1937@gmail.com>
2020-01-10 18:06:06 +0200

    Added: project __init__.py

---
+++ ovl/__init__.py
@@ -0,0 +1,35 @@
+from .camera_.camera import Camera
+from .camera_.camera_calibration import CameraCalibration
+from .camera_.camera_settings import CameraSettings
+from .connections import *
+from .connections.network_location import NetworkLocation
+from .connections.network_tables_connection import NetworkTablesConnection
+from .connections.serial_connection import SerialConnection
+from .contour_filters_.conditional_contour_filter import conditional_contour_filter
+from .contour_filters_.contour_filter import contour_filter
+from .contour_filters_.contour_filters import *
+from .contour_filters_.sorters import *
+from .directions_.directing_functions import *
+from .directions_.direction_monitors.direction_monitor import DirectionMonitor
+from .directions_.direction_monitors.stop_if_close_monitor import StopIfCloseMonitor
+from .directions_.director import Director
+from .display_image.display_contours import display_contours
+from .display_image.display_image import display_image
+from .exceptions_ import exceptions
+from .hsv_calibration_.hsv_calibration import HSVCalibration
+from .image_filters_.image_filters import *
+from .image_utilities import drawing
+from .image_utilities.naming_conventions import *
+from .image_utilities.photo_array import photo_array
+from .math_ import geometry
+from .morphological_functions_.morphological_functions import erosion, dilation
+from .thresholds.canny_edge import CannyEdgeThreshold
+from .thresholds.color_.built_in_colors import *
+from .thresholds.color_.color import Color
+from .thresholds.color_.multi_color import MultiColor
+from .thresholds.threshold import Threshold
+from .visions.ambient_vision import AmbientVision
+from .visions.multi_vision import MultiVision
+from .visions.vision import Vision
+
+__version__ = '2020.1.0'
