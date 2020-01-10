c5ca9a08107c6a0ca2ba86a9ad56b63fab90d17b
1937Elysium <frcelysium1937@gmail.com>
2020-01-10 17:54:56 +0200

    Implemented: conditional_contour_filter, a decorator used to create a contour filter from a single condition that behaves like a contour_filter

---
+++ ovl/contour_filters_/conditional_contour_filter.py
@@ -0,0 +1,32 @@
+from typing import List
+import numpy as np
+import functools
+
+from ..partials.reverse_partial import ReversePartial
+
+
+def _loaded_condition(loaded_contour_filter, contour_list: List[np.ndarray]):
+    return list(filter(loaded_contour_filter, contour_list))
+
+
+def conditional_contour_filter(single_contour_filter):
+    """
+    A contour_filter that turns a function that filters a single contour (and returns true if it has passed)
+    to an iterative one that goes over a list
+
+    example:
+        @conditional_contour_filter
+        def area_filter(contour, minimum_area=50)
+            return cv2.contourArea(contour) > minimum_area
+
+    This function can then be passed to a Vision object:
+        filters = [area_filter(60)] # sets the minimum area to 60 -> the first parameter passed
+        vision = Vision(..., contour_filters=filters, ...)
+
+    :param single_contour_filter:
+    :return:
+    """
+
+    def argument_loader(*args, **kwargs):
+        return functools.partial(_loaded_condition, ReversePartial(single_contour_filter, *args, **kwargs))
+    return argument_loader
