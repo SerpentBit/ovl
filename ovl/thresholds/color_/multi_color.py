62e438d0e8a982f1a1b0390fe624263b7e6661e2
1937Elysium <frcelysium1937@gmail.com>
2020-01-17 14:06:35 +0200

    Implemented: MultiColor, a threshold used to create a binary image from a non-continuous hsb(hsv) range

---
+++ ovl/thresholds/color_/multi_color.py
@@ -0,0 +1,58 @@
+# Copyright 2018-2019 Ori Ben-Moshe - All rights reserved.
+import cv2
+import numpy as np
+
+from .color import Color
+from ..threshold import Threshold
+
+
+class MultiColor(Threshold):
+    """
+    A Threshold that uses multiple
+    """
+    def __init__(self, colors):
+        """
+        :param colors: A list of Color objects or tuple that can be turned into a color object
+        Example:
+              MultiColor([Color([15, 50, 50], [45, 255, 125]),
+                         (75, 100),
+                         [[155, 50, 50], 175],
+                         [[105, 175, 25], [115, 255, 255]])
+         These are different ways to create ranges of colors
+         """
+        self.colors = colors
+        for index, color in enumerate(colors):
+            if type(color) in (list, tuple):
+                self.colors[index] = Color(*color)
+
+    def convert(self, image: np.ndarray):
+        """
+        Thresholds an image using the color ranges,
+        all pixels that are not in the color ranges defined are set to black (0, 0, 0)
+        and all others to white (255, 255, 255)
+        """
+        if self.colors is None:
+            raise ValueError("Cannot convert an image to a binary, no colors given.")
+        if len(self.colors) == 0:
+            raise ValueError("Cannot convert an image to binary, no colors given.")
+        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        binary_image = self.colors[0].threshold(hsv_image)
+        if len(self.colors) < 1:
+            return binary_image
+        for color in self.colors[1:]:
+            binary_image = cv2.bitwise_or(binary_image, color.threshold(hsv_image))
+
+        return binary_image
+
+    def validate(self, *args, **kwargs) -> bool:
+        for color in self.colors:
+            if type(color) not in Color:
+                if not color.validate():
+                    return False
+        return True
+
+    def __str__(self):
+        return ', '.join([str(color) for color in self.colors])
+
+    def __repr__(self):
+        return'MultiColor(0)'.format([repr(i) for i in self.colors])
