3a9e8a1fbcb6311b927b17275494b99e07499c2f
Elysium 1937 <45563197+1937Elysium@users.noreply.github.com>
2020-01-27 18:15:18 +0200

    Created yellow circle detection example

---
+++ code examples/simple_yellow_circle_pipeline.py
@@ -0,0 +1,22 @@
+import ovl
+
+# filter contours that are larger than 200 pixels
+# and are approximately a circle and then sort by size
+
+contour_filters = [ovl.area_filter(min_area=200), ovl.circle_filter(min_area_ratio=0.7), ovl.dec_area_sort()] 
+
+threshold = ovl.YELLOW_HSV # Define the wanted color to detect 
+camera = ovl.Camera(0) # open the first connected camera 
+
+yellow_circle = Vision(threshold=threshold,
+                       contour_filters=contour_filters,
+                       camera=camera,
+                       image_filters=image_filters)
+
+while True:
+    image = yellow_circle.get_filtered_image()
+    contours, filtered_image = yellow_circle.detect(image)
+    directions = yellow_circle.get_directions(contours, image)
+    
+    print(directions) # prints out the (x, y) coordinates of the largest target
+
