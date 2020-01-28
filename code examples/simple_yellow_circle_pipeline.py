a756fd5ba914caf7fcc5f2f3374881e6972e1094
Elysium 1937 <45563197+1937Elysium@users.noreply.github.com>
2020-01-28 18:28:52 +0200

    yellow circle example camera declaration moved to Vision declaration

--- code examples/simple_yellow_circle_pipeline.py
+++ code examples/simple_yellow_circle_pipeline.py
@@ -6,17 +6,16 @@ import ovl
 contour_filters = [ovl.area_filter(min_area=200), ovl.circle_filter(min_area_ratio=0.7), ovl.dec_area_sort()] 
 
 threshold = ovl.YELLOW_HSV # Define the wanted color to detect 
-camera = ovl.Camera(0) # open the first connected camera 
 
 yellow_circle = Vision(threshold=threshold,
                        contour_filters=contour_filters,
-                       camera=camera,
+                       camera=0, # open the first connected camera
                        image_filters=image_filters)
 
 while True:
     image = yellow_circle.get_filtered_image()
     contours, filtered_image = yellow_circle.detect(image)
-    directions = yellow_circle.get_directions(contours, image)
+    directions = yellow_circle.get_directions(contours, filtered_image)
     
     print(directions) # prints out the (x, y) coordinates of the largest target
 
