b3c627551e439cb15d9390a2e47d7be228b3fe9d
1937Elysium <frcelysium1937@gmail.com>
2020-03-18 17:20:25 +0200

    Copyright removed from most files, corrected in LICENSE

--- ovl/directions_/directing_functions.py
+++ ovl/directions_/directing_functions.py
@@ -1,4 +1,3 @@
-# Copyright 2018-2019 Ori Ben-Moshe - All rights reserved.
 from numpy import ndarray
 
 from ..math_ import geometry
@@ -9,7 +8,7 @@ def xy_center_directions(contours, image: ndarray):
     Returns the director for the robot for the given contours for the x and y axis
     :param contours: the final contours found after filtering - your targets
     :param image: the image from which it was found
-    :return: the director
+    :return: the normalized screen space x and y coordinates of your final targets
     """
     height, width, _ = image.shape
     x, y = geometry.contour_average_center(contours)
@@ -18,41 +17,52 @@ def xy_center_directions(contours, image: ndarray):
     return x - 1, y - 1
 
 
-def y_center_directions(contour, image: ndarray):
+def y_center_directions(contours, image: ndarray):
     """
     Returns the director for the robot for the given contours for the y axis only
     the value of the direction is between -1 and 1
-    :param contour: the final contours - your targets
+    :param contours: the final contours - your targets
     :param image: the image from which it was found
-    :return: the direction to move in the y axis (-1 to 1)
+    :return: the normalized screen space y coordinate of your final targets
     """
     height, _, _ = image.shape
-    _, y = geometry.contour_average_center(contour)
+    _, y = geometry.contour_average_center(contours)
     y /= height / 2
     return y - 1
 
 
-def x_center_directions(contour, image: ndarray):
+def x_center_directions(contours, image: ndarray):
     """
     Returns the director for the robot for the given contours for the x axis only
-    :param contour: the final contours - your targets
+    :param contours: the final contours - your targets
     :param image: the image from which it was found
-    :return: the direction to move in the y axis (-1 to 1)
+    :return: the normalized screen space x coordinate of your final targets
     """
     _, width, _ = image.shape
-    x, _ = geometry.contour_average_center(contour)
+    x, _ = geometry.contour_average_center(contours)
     x /= width / 2
     return x - 1
 
 
-def center_directions(contour, image: ndarray):
+def center_directions(contours, image: ndarray):
     """
     Returns the average center of the contours
     or list of contours that are the final targets
 
     This is the default directions function since it
     doesnt calculate any directions, only finds the center
-    :param contour: the final contours - your targets
+    :param contours: the final contours - your targets
     :param image: the image from which it was found
     """
-    return geometry.contour_average_center(contour)
+    return geometry.contour_average_center(contours)
+
+
+def target_amount_directions(contours, image: ndarray):
+    """
+    Counts the amount of successful object detections.
+    :param contours: the final contours - your targets
+    :param image: the image from which it was found
+    :return: the amount of targets detected
+    """
+    return len(contours)
+
