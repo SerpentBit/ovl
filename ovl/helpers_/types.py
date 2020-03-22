0f1a5494372411581bfb3342ff985f1e55eafff6
1937Elysium <frcelysium1937@gmail.com>
2020-03-22 18:29:20 +0200

    Created: NumberRange type hint creator

--- ovl/helpers_/types.py
+++ ovl/helpers_/types.py
@@ -1,3 +1,12 @@
 import typing
 
 VisionLike = typing.Union["Vision", "AmbientVision"]
+
+
+def RangedNumber(start, end):
+    class Hint:
+        def __class_getitem__(cls, item):
+            cls.start = item[0]
+            cls.end = item[1]
+            return cls
+    return typing.Union[float, Hint[start, end]]
