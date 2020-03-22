b634831c0d3963b5c6d0d19d79e3ccf2fc835cff
1937Elysium <frcelysium1937@gmail.com>
2020-03-22 19:20:55 +0200

    Added: CONTOUR_FILTERS to __init__.py

--- ovl/contour_filters_/__init__.py
+++ ovl/contour_filters_/__init__.py
@@ -1,5 +1,5 @@
 from .shape_filters import *
 from .conditional_contour_filter import conditional_contour_filter
-from .contour_filter import contour_filter
+from .contour_filter import contour_filter, CONTOUR_FILTERS
 from .sorters import *
 from .contour_filters import *
