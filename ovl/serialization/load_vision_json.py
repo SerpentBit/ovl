73ce34aec0bca3124e092255037d87313668038c
1937Elysium <frcelysium1937@gmail.com>
2021-03-27 12:16:01 +0300

    Added: polygon filter constants

--- ovl/serialization/load_vision_json.py
+++ ovl/serialization/load_vision_json.py
@@ -1,7 +1,19 @@
+import enum
+
+
+class SerializeType(enum):
+    Generic = 0
+    Iterable = 1
+    Object = 2
+    FilterFunction = 3
+    Device = 4
+
+
 def validate_loaded_vision(loaded_vision):
     """
-     Verifies the a loaded vision (Dictionary) contains the the necessary
+    Verifies the a loaded vision (Dictionary) contains the the necessary
             Attributes to create a Vision object
+
     :param loaded_vision: A dictionary containing raw data of the Vision object
     :return:
     """
@@ -11,6 +23,7 @@ def validate_loaded_vision(loaded_vision):
 def loads_vision(serialized_vision):
     """
     Loads a Vision object from a json string.
+
     :param serialized_vision:
     :return:
     """
@@ -22,10 +35,19 @@ def loads_vision(serialized_vision):
 
 def load_vision(vision_file):
     """
-        Loads a Vision object from a json string.
-        :param vision_file: path to the vision file (a json file)
-        :return:
-        """
+    Loads a Vision object from a json string.
+
+    :param vision_file: path to the vision file (a json file)
+    :return:
+    """
     raise NotImplementedError()
     # with open(vision_file, "r") as vision:
     #     return load_serialized_vision(loads(vision.read()))
+
+
+def dumps_vision(vision):
+    vision_dictionary = {}
+
+
+def dump_vision(file, vision):
+    pass
