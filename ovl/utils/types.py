0ac458a955f56db6108098487927fc273b0b01b5
1937Elysium <frcelysium1937@gmail.com>
2021-06-16 00:29:42 +0300

    Changed: RangedNumber.Hint.__class_getitem__ self parameter to cls

--- ovl/utils/types.py
+++ ovl/utils/types.py
@@ -10,10 +10,10 @@ Target = Union[np.ndarray, Tuple, Any]
 
 def RangedNumber(start, end):
     class Hint:
-        def __class_getitem__(self, item):
-            self.start = item[0]
-            self.end = item[1]
-            return self
+        def __class_getitem__(cls, item):
+            cls.start = item[0]
+            cls.end = item[1]
+            return cls
     return Union[float, Hint[start, end]]
 
 
