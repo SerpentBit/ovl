8012d79c7e63e7c8e53f333b3d3d701090004170
1937Elysium <frcelysium1937@gmail.com>
2022-02-13 01:10:29 +0200

    Fixed: setup.py extras_require

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/SerpentBit/ovl",
     install_requires=["numpy"],
-    extra_require=extra_requirements,
+    extras_require=extra_requirements,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Education",
