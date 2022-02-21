338e9b917733ca59db8530b343d7ccb229b3156c
1937Elysium <frcelysium1937@gmail.com>
2022-02-21 22:39:34 +0200

    Updated: simple code example

--- code examples/vision pipeline examples/simple_yellow_circle_pipeline.py
+++ code examples/vision pipeline examples/simple_yellow_circle_pipeline.py
@@ -1,10 +1,11 @@
 import ovl
+from ovl import display_contours
 
-target_filters = [ovl.percent_area_filter(min_area=0.005),
-                  ovl.circle_filter(min_area_ratio=0.7),
+target_filters = [ovl.percent_area_filter(minimal_percent=0.5),
+                  ovl.circle_filter(min_area_ratio=0.5),
                   ovl.area_sort()]
 
-threshold = ovl.Color([20, 100, 100], [55, 255, 255])
+threshold = ovl.Color([20, 50, 50], [55, 255, 255])
 
 yellow_circle = ovl.Vision(threshold=threshold,
                            target_filters=target_filters,
@@ -15,5 +16,5 @@ while True:
     image = yellow_circle.get_image()
     targets, filtered_image = yellow_circle.detect(image)
     directions = yellow_circle.get_directions(targets, filtered_image)
-
+    display_contours(filtered_image, targets, display_loop=True)
     print(directions)  # prints out the (x, y) coordinates of the largest target
