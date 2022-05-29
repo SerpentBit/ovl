7bf770f2d9ba58e1de7680acb611426d2096ec9c
1937Elysium <frcelysium1937@gmail.com>
2022-05-29 23:49:34 +0300

    Typo: in camera_configuration.py typo

--- code examples/camera examples/camera_configuration.py
+++ code examples/camera examples/camera_configuration.py
@@ -5,14 +5,14 @@ camera = ovl.Camera(0)
 
 camera_config = CameraConfiguration({
     CameraProperties.IMAGE_WIDTH: 640,  # set image width to 640 pixels
-    CameraProperties.IMAGE_HEIGHT: 480,  # set image width to 640 pixels
+    CameraProperties.IMAGE_HEIGHT: 480,  # set image width to 480 pixels
     CameraProperties.CAMERA_FPS: 60,  # Frame per second to 60
     CameraProperties.AUTO_EXPOSURE: 0  # Turn off auto exposure
 })
 
 camera.configure_camera(camera_config)
 
-camera.set_exposure(-100, configuration_delay=2)
+camera.set_exposure(-100, configuration_delay=2)  # Setting exposure usually demands a delay for adjustment
 
 while True:
     image = camera.get_image()
